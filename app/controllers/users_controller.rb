29d2cfc50885f0206a9510d17a0b389b14bd9297
EC2 Default User <ec2-user@ip-172-31-37-70.ap-northeast-1.compute.internal>
2020-03-25 14:35:42 +0000

    プロフィール編集機能とスワイプ機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.where.not(id: current_user.id)
+  end
+  
   def show
     @user = User.find_by(id: params[:id])
   end
   
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :img_name )
-  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :img_name )
+    end
 end
