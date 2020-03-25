4f61c330e1ad5a2ef0f2d5c2857d1be0f9fce4bc
EC2 Default User <ec2-user@ip-172-31-37-70.ap-northeast-1.compute.internal>
2020-03-25 15:23:34 +0000

    マッチング機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'matching/index'
   devise_for :users
   root 'top#index'
   
   resources :users, only: %i(index show)
+  resources :reactions, only: %i(create)
+  resources :matching, only: %i(index)
 end
